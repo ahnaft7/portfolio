4698f022b046e69b3adc133fe29ba44ac3540e9e
Ahnaf Tajwar <tajwara@bu.edu>
2024-07-24 04:59:50 -0400

    Changed text in bentogrid

--- data/index.ts
+++ data/index.ts
@@ -18,7 +18,7 @@ export const gridItems = [
 },
 {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: "Based in NYC, delivering global impact",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -38,7 +38,7 @@ export const gridItems = [
 },
 {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Tech enthusiast with a passion for development",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
