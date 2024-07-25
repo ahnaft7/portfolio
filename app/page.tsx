7c8c94a177390896b3ed1fcd8fb89bd7fb08bf44
Ahnaf Tajwar <tajwara@bu.edu>
2024-07-25 05:30:26 -0400

    Completed project section and adjusted navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import Image from "next/image";
 import Hero from "@/components/Hero"
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import { Grid } from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex min-h-screen flex-col items-center justify-between p-24">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
